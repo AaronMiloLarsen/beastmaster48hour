025bcf58eccfaeda5e7383d30f33c6df5116e485
Sarah Tirey <sarah@email.com>
2020-11-03 06:11:59 -0500

    third commit added temp convert button

--- src/components/apps/Weather/WeatherApp.js
+++ src/components/apps/Weather/WeatherApp.js
@@ -1,45 +1,78 @@
 import React, { useState } from 'react';
-
+// import PropTypes from 'prop-types';
 
 
 
 const WeatherApp = (props) => {
 
-const key = 'c570150f64f3e89bcc9f5861600d77b9'
+    const key = 'c570150f64f3e89bcc9f5861600d77b9'
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${props.lat}&lon=${props.long}&appid=${key}`
+    const [weather, setWeather] = useState('');
 
-const url = `http://api.openweathermap.org/data/2.5/weather?lat=${props.lat}&lon=${props.long}&appid=${key}`
-
-const [weather, setWeather] = useState('');
-
-fetch(url)
+    fetch(url)
 
         .then((res) => res.json())
         .then((json) => setWeather(json.main.temp))
         .catch((err) => console.log(err));
-        // .then(res => res.json())
-        // .then(json => console.log(json))         
-        // .then(json => displayWeather(json))
-        // .catch(err => console.log(err));
-
-        // console.log(`This is the url: ${url}.`);
-        // console.log(`The longitude is: ${props.long}.`);
-        // console.log(`The latitude is: ${props.lat}.`);
-
-        // function displayWeather(json){
-        //     let weather = json.;
-            // let temp = weather.main;
+        console.log(weather); 
         
-            console.log(weather); 
-        // }
    
         /* When the input field receives input, convert the value from fahrenheit to celsius */
-        const fahrenheit = Math.round((weather - 273.15)*1.8 + 32);
+        const fahrenheit = Math.round((weather - 273.15)*1.8 + 32) ;
         const celcius = Math.round((weather - 273.15));
+
+        const [ color, setColor ] = useState('blue');
+        const [ display, setDisplay ] = useState('inline-block');
+        const [ temp, setTemp ] = useState(fahrenheit);
+
+        let styles = {
+            color: color,
+            display : display,
+            temp: temp
+        };
+// using a ternary operator to check what current value is, and setting it accordingly
+        const toggleColor = () => {
+            color === 'blue' ? setColor('orange') : setColor('blue');
+        }
+
+        const toggleTemp = () => {
+            temp === fahrenheit ? setTemp(celcius) : setTemp(fahrenheit);
+
+        }
         return (
-            <div>
-                <h2>{fahrenheit + '\u00B0 F'}<br /> {celcius + '\u00B0 C'}</h2>
+            <div className="main">
+                <div className="mainDiv">
+                    <div style={styles}>
+                        <FunctionalComponent string={fahrenheit + '\u00B0 F'} function={toggleTemp} selectedStyle={ temp }/>
+
+                        {/* <FunctionalComponent string={celcius + '\u00B0 C'} function={toggleColor} /> */}
+
+                {/* <h2>{fahrenheit + '\u00B0 F'} fahrenheit
+                <br /> 
+                {celcius + '\u00B0 C'} celsius</h2> */}
+                
+                    </div>
+                </div>
             </div>
         )
 }
 
 export default WeatherApp;
+
+const FunctionalComponent = (props) => {
+    return (
+        <div>
+            <p>{props.string}</p>
+            <button onClick={props.function}>fahrenheit or celsius?</button>
+            <TempChangeComponent selectedStyle={ props.selectedStyle } />
+        </div>
+    )
+}
+
+const TempChangeComponent = (props) => {
+    return (
+        <div>
+            <p>The current temperature is : { props.selectedStyle }</p>
+        </div>
+    )
+}
